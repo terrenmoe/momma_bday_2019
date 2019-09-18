315310a815148f9bf09a156861b39da18ffcfd34
Alex Rossiter <terrenmoe@gmail.com>
2019-09-18 11:39:07 -0700

    Made some changes from template

--- src/App.js
+++ src/App.js
@@ -5,19 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        <h1 className="Heading">Happy Birthday Momma</h1>
+        <img src={logo} className="Logo Slider" alt="logo" />
       </header>
     </div>
   );
